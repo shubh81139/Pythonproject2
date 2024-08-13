c4f91a8571b7874b7d774b3f75c6a255003f3ccd
SHUBHAM <123285766+shubh81139@users.noreply.github.com>
2024-08-14 01:01:42 +0530

    Add files via upload

---
+++ digitalclock.py
@@ -0,0 +1,16 @@
+from tkinter import Tk            
+from tkinter import Label
+import time
+
+root = Tk()
+root.title("Clock")
+
+def present_time():
+    display_time = time.strftime("%I:%M:%S %p")
+    digi_clock.config(text=display_time)
+    digi_clock.after(200,present_time)
+
+digi_clock = Label(root , font=("arial",150),bg="beige",fg="brown")
+digi_clock.pack()
+present_time()
+root.mainloop()
